428559a082887064445a35655eb299eb0fe99fb8
aniruxh004 <131795735+aniruxh004@users.noreply.github.com>
2025-07-12 14:56:55 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ connectDb('mongodb://127.0.0.1:27017/ai-notes-summariser').then(()=>console.log(
 
 
 app.use(cors({
-  origin: 'http://localhost:5174',   
+  origin: 'https://ai-notes-summariser-dep-client.onrender.com',   
   methods: ['GET', 'POST', 'OPTIONS'],
   credentials: true,                 
   allowedHeaders: ['Content-Type'],
